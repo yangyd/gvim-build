047946aa65ac136ff96d6b3d9bfbf1a5d3b6ce67
Yuedong Yang <yangyd@live.com>
2014-10-11 16:33:17 +0800

    add script that register "Edit with gvim" command to the shell context menu

---
+++ sbin_mingw/gvim-context-register.bat
@@ -0,0 +1,10 @@
+@echo off
+
+reg delete HKCR\*\shell\edit_with_gvim /f
+
+reg add HKCR\*\shell\edit_with_gvim
+reg add HKCR\*\shell\edit_with_gvim /t REG_SZ /d "Edit with gvim" /f
+
+set gvim_cmd="\"%~dp0gvim.cmd\" \"%%1\""
+reg add HKCR\*\shell\edit_with_gvim\command
+reg add HKCR\*\shell\edit_with_gvim\command /t REG_SZ /d %gvim_cmd% /f
